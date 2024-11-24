a49a44954860ed3c709808a5b9554438883386c2
Harshul Raina <140810792+DrMadgax@users.noreply.github.com>
2024-11-24 16:02:41 +0530

    nodb 2

--- db.php
+++ db.php
@@ -1,8 +1,5 @@
 <?php
-    $con = mysqli_connect("localhost","root","","travel_db");
-    if (!$con){
         $con = mysqli_connect("localhost","root","");
-        mysqli_query($con,"CREATE DATABASE travel_db");
+        mysqli_query($con,"CREATE DATABASE IF NOT EXISTS travel_db");
         mysqli_query($con,"USE travel_db");
-    }
 ?>
